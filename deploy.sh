745b6bd5babca41a6a352755735c2ea4ffd0d87b
sadie <sgill@flowwest.com>
2019-03-13 14:37:44 -0700

    update deploy script to point to correct s3 bucket

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ hugo
 ​
 cd public
 echo "Syncing to amazon s3 bucket..."
-aws s3 sync . s3://delta.scienceintegrationteam.com
+aws s3 sync . s3://cvpia.scienceintegrationteam.com
 ​
 cd ..
 ​
 echo ""
-echo "site is available at: http://delta.scienceintegrationteam.com"
+echo "site is available at: http://cvpia.scienceintegrationteam.com"
 echo ""
